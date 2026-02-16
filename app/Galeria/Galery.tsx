0c0e9eac0793f92e9f394cc4b2e934a04f01ffbe
Enriquebc13 <baezenrique114@gmail.com>
2026-02-16 12:54:36 -0600

    Update Galery.tsx

--- app/Galeria/Galery.tsx
+++ app/Galeria/Galery.tsx
@@ -1,5 +1,5 @@
 import ImageGalery from "@/components/imageGalery/imageGalery";
-import ImagePickerView from "@/components/imagePicker/imagePicker";
+
 
 export default function Galeria(){
     return (
