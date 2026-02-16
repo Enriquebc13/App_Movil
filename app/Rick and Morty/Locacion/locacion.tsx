04a8048c974be98220880af42f2d308d07804157
Enriquebc13 <baezenrique114@gmail.com>
2026-02-16 12:57:29 -0600

    Update locacion.tsx

--- app/Rick and Morty/Locacion/locacion.tsx
+++ app/Rick and Morty/Locacion/locacion.tsx
@@ -1,5 +1,5 @@
 import LocationNavegacion from "@/components/Rick-Morty/locations/navigationLocations";
-import NavigationLocation from "@/components/Rick-Morty/locations/navigationLocations";
+
 
 export default function Locacion2(){
     return (
